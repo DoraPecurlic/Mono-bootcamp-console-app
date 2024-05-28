e867757e52db46b8f366925d5884416ed36f84a4
DoraPecurlic <dora.pecurlic01@gmail.com>
2024-05-28 22:56:47 +0200

    modified program file

---
+++ FlowerAsistent/FlowerAsistent/Cart.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace FlowerAsistent
+{
+    public class Cart
+    {
+        public List<Plant> flowers = new List<Plant>();
+
+        public void AddFlower(Plant plant)
+        {
+            this.flowers.Add(plant);
+        }
+
+        public void RemoveFlowerByIndex(int index)
+        {
+            if (index >= 0 && index < flowers.Count)
+            {
+                flowers.RemoveAt(index);
+            }
+            else
+            {
+                Console.WriteLine("Invalid index. Please try again.");
+            }
+        }
+
+        public void DisplayCart()
+        {
+            for (int i = 0; i < flowers.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}. {flowers[i].Color} {flowers[i].Specie}");
+            }
+        }
+    }
+}
